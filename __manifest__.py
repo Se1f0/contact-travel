c76ceef792b7f5b072f9d9bb977066106ad83e49
SAFE <sesegueni@gmail.com>
2023-10-31 20:33:47 +0100

    Create basic module named contact_travel

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Contact Travel",
+    'summary': "Module personnalisé de gestion des contacts liés aux voyages",
+    'description': "Ce module vous permet de gérer les contacts liés aux voyages avec un système automatique de niveau de récompense.",
+    'author': "Seif Eddine Segueni",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'sequence': -100,
+}
