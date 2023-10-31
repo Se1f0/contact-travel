8af8a00108890aea1511de9ed939342e3a1a3c40
SAFE <sesegueni@gmail.com>
2023-10-31 21:24:07 +0100

    Add relationship between Voyage and contact

--- models/voyage.py
+++ models/voyage.py
@@ -7,3 +7,5 @@ class Voyage(models.Model):
     nom = fields.Char(string='Nom du voyage', required=True)
     date_de_depart = fields.Date(string='Date de départ' ,required=True)
     destination = fields.Char(string='Destination' ,required=True)
+    # This field will link the voyage and contact models together using the Many2One relationship
+    contact_id = fields.Many2one('res.partner', string='Contact' ,required=True)
